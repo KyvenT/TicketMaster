4aa2c102bec2245045def98b0fc2b8f229f44b18
Martin Shaker <mshaker7@uwo.ca>
2024-03-31 15:29:37 -0400

    Created checklist combo box

---
+++ TicketMaster/src/MultiSelectComboBox.cpp
@@ -0,0 +1,41 @@
+#include "MultiSelectComboBox.h"
+#include <QStandardItem>
+#include <QLineEdit>
+#include <QListView>
+
+MultiSelectComboBox::MultiSelectComboBox(QWidget *parent) : QComboBox(parent)
+{
+    model = new QStandardItemModel(this);
+    setModel(model);
+    setView(new QListView(this));
+
+    // Ensure the combo box doesn't close on item selection
+    view()->viewport()->installEventFilter(this);
+
+    // Optional: Configure the line edit for displaying selected items (if you plan to use it)
+    setEditable(true);
+    lineEdit()->setReadOnly(true);
+    lineEdit()->setCursor(Qt::ArrowCursor);
+}
+
+QStringList MultiSelectComboBox::selectedItems() const
+{
+    QStringList selections;
+    for (int i = 0; i < model->rowCount(); ++i) {
+        QStandardItem *item = model->item(i);
+        if (item->checkState() == Qt::Checked) {
+            selections.append(item->text());
+        }
+    }
+    return selections;
+}
+
+void MultiSelectComboBox::setSelectedItems(const QStringList &items)
+{
+    for (int i = 0; i < model->rowCount(); ++i) {
+        QStandardItem *item = model->item(i);
+        item->setCheckState(items.contains(item->text()) ? Qt::Checked : Qt::Unchecked);
+    }
+}
+
+
---
+++ TicketMaster/src/MultiSelectComboBox.h
@@ -0,0 +1,31 @@
+#ifndef TICKETMASTER_MULTISELECTCOMBOBOX_H
+#define TICKETMASTER_MULTISELECTCOMBOBOX_H
+#ifndef MULTISELECTCOMBOBOX_H
+#define MULTISELECTCOMBOBOX_H
+
+#include <QComboBox>
+#include <QStandardItemModel>
+
+class MultiSelectComboBox : public QComboBox
+{
+Q_OBJECT
+
+public:
+    explicit MultiSelectComboBox(QWidget *parent = nullptr);
+
+    // Utility functions to manage selections
+    QStringList selectedItems() const;
+    void setSelectedItems(const QStringList &items);
+
+private:
+    QStandardItemModel *model;
+};
+
+#endif // MULTISELECTCOMBOBOX_H
+
+
+#endif // MULTISELECTCOMBOBOX_H
+
+
+
+#endif //TICKETMASTER_MULTISELECTCOMBOBOX_H
